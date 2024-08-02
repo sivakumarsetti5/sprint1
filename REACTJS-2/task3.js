7c4f117b165d73532a8f981097f3587336f1c785
sivakumarsetti5 <sivakumarsetti5@gmail.com>
2024-08-02 21:24:03 +0530

    first commit

---
+++ REACTJS-2/task3.js
@@ -0,0 +1,12 @@
+// Write a function to check the given number if even or odd.
+
+function fnCheck(no){
+   if(no%2==0){
+    console.log('Even number')
+   }
+   else{
+    console.log('Odd number')
+   }
+}
+
+fnCheck(5)
